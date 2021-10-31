378dfa2af4469a2bf590387b860193f3e3065896
magamex <magamex.22@gmail.com>
2021-10-31 18:02:41 -0300

    test 2

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { useState } from 'react'
+import React, { useState} from 'react';
+import Router from 'next/router';
 
 const axios = require('axios');
 
